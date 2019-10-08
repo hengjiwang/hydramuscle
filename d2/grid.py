26ba82d650ee1789666474dca30e4568bffc566d
hengjiwang <hengjw@uw.edu>
2019-10-08 10:48:37 -0700

    Optimize with numba.jitclass

--- d2/grid.py
+++ d2/grid.py
@@ -10,28 +10,196 @@ import time
 from scipy.integrate import odeint
 from scipy.sparse import spdiags
 import pandas as pd
-from cell import Cell
-from numba import jitclass, int32, float32
+# from cell import Cell
+from numba import jitclass, int32, float64
+
+spec = [('k1', float64),
+('k2', float64),
+('ka', float64),
+('kip', float64),
+('k3', float64),
+('v40', float64),
+('v41', float64),
+('kr', float64),
+('k5', float64),
+('k6', float64),
+('ki', float64),
+('kg', float64),
+('a0', float64),
+('v7', float64),
+('v8', float64),
+('kca', float64),
+('k9', float64),
+('beta', float64),
+('c0', float64),
+('s0', float64),
+('r0', float64),
+('ip0', float64),
+('T', float64),
+('dt', float64),
+('time', float64[:]),
+('c_m', float64),
+('A_cyt', float64),
+('V_cyt', float64),
+('d', float64),
+('F', float64),
+('v0', float64),
+('n0', float64),
+('hv0', float64),
+('hc0', float64),
+('x0', float64),
+('z0', float64),
+('p0', float64),
+('q0', float64),
+('bx0', float64),
+('cx0', float64),
+('g_cal', float64),
+('e_cal', float64),
+('k_cal', float64),
+('g_cat', float64),
+('e_cat', float64),
+('g_kcnq', float64),
+('e_k', float64),
+('g_kv', float64),
+('g_kca', float64),
+('g_bk', float64),
+('e_bk', float64),
+('gc', float64),
+('g_ip3', float64),
+('num', int32),
+('num2', int32),
+('L', float64[:,:]),
+('scale_stim_v', float64),
+('scale_stim_ip', float64),
+('r_inc', float64),
+('k1p', float64),
+('k1n', float64),
+('k2p', float64),
+('k2n', float64),
+('k3p', float64),
+('k3n', float64),
+('k4p', float64),
+('k4n', float64),
+('g0', float64),
+('c1g0', float64),
+('c2g0', float64),
+('c3g0', float64),
+('c4g0', float64),
+('phi0', float64),
+('phi1', float64),
+('phi2', float64),
+('phi3', float64),
+('phi4', float64)]
+
+num = 50
+
+onex = np.ones(num)
+Dx = spdiags(np.array([onex,-2*onex,onex]), np.array([-1,0,1]),num,num).toarray()
+Dx[0,0] = -1
+Dx[num-1,num-1] = -1 
+Ix = np.eye(num)
+L = np.kron(Dx, Ix) + np.kron(Ix, Dx)
 
 @jitclass(spec)
-class Grid(Cell):
+class Grid():
     '''A 2D dynamical model with cells connected by gap junctions'''
-    def __init__(self, num=10, T=100, k2=0.2, s0=600, d=10e-4, v7=0, k9=0.01, scale_stim_v = 0.01, scale_stim_ip = 1.0):
-
+    def __init__(self, num=num, T=100, dt = 0.01, k2=0.2, s0=600, d=10e-4, v7=0, k9=0.01, scale_stim_v = 0.01, scale_stim_ip = 1.0, L=L):
+        
         # General parameters
-        super().__init__(T)
         self.gc = 5e4
         self.g_ip3 = 1
+
+        # Hofer parameters
+        self.k1 = 0.0004
+        self.k2 = 0.08
+        self.ka = 0.2
+        self.kip = 0.3
+        self.k3 = 0.5
+        self.v40 = 0.025
+        self.v41 = 0.2
+        self.kr = 1
+        self.k5 = 0.5
+        self.k6 = 4
+        self.ki = 0.2
+        self.kg = 0.1 # unknown
+        self.a0 = 1 # 1e-3 - 10
+        self.v7 = 0.04 # 0 - 0.05
+        self.v8 = 4e-4
+        self.kca = 0.3
+        self.k9 = 0.08
+        self.beta = 20
+
+        self.c0 = 0.05
+        self.s0 = 60
+        self.r0 = 0.94
+        self.ip0 = 0.01
+
+        # Fast Parameters
+        self.c_m = 1e-6 # [F/cm^2]
+        self.A_cyt = 4e-5 # [cm^2]
+        self.V_cyt = 6e-9 # [cm^3]
+        self.d = 10e-4 # 10e-4 # [cm]
+        self.F = 96485332.9 # [mA*s/mol]
+        self.v0 = -50 # (-40 to -60)
+        self.n0 = 0
+        self.hv0 = 0
+        self.hc0 = 0
+        self.x0 = 0
+        self.z0 = 0
+        self.p0 = 0
+        self.q0 = 0
+        self.bx0 = 0
+        self.cx0 = 0
+
+        # Fluorescence Parameters
+        self.r_inc = 200
+        self.k1p = 2.5
+        self.k1n = 0.1
+        self.k2p = 16.9
+        self.k2n = 205
+        self.k3p = 1.1
+        self.k3n = 11.8
+        self.k4p = 1069
+        self.k4n = 5.8
+        self.g0 = 3.281
+        self.c1g0 = 4.101
+        self.c2g0 = 0.017
+        self.c3g0 = 0
+        self.c4g0 = 0.0007
+        self.phi0 = 1
+        self.phi1 = 1
+        self.phi2 = 1
+        self.phi3 = 1
+        self.phi4 = 81
+        
+        # CaL parameters
+        self.g_cal = 0.0005 # [S/cm^2] 
+        self.e_cal = 51
+        self.k_cal = 1 # [uM]
+
+        # CaT parameters
+        self.g_cat = 0.003 # 0.0003
+        self.e_cat = 51
+
+        # KCNQ parameters
+        self.g_kcnq = 0 # 0.0001 # [S/cm^2]
+        self.e_k = -75 
+
+        # Kv parameters
+        self.g_kv = 0 # 0.0004
+
+        # BK parameters
+        self.g_kca =  10e-9 / self.A_cyt # 45.7e-9 / self.A_cyt
+
+        # Background parameters
+        self.g_bk = 0
+        self.e_bk = -55
+
 
         # Build grid
         self.num = num
         self.num2 = self.num ** 2
-        onex = np.ones(self.num)
-        Dx = spdiags(np.array([onex,-2*onex,onex]), np.array([-1,0,1]),self.num,self.num).toarray()
-        Dx[0,0] = -1
-        Dx[self.num-1,self.num-1] = -1 
-        Ix = np.eye(self.num)
-        self.L = np.kron(Dx, Ix) + np.kron(Ix, Dx)
+        self.L = L
 
         # Modified parameters
         # self.ip_decay = 0.04
@@ -42,6 +210,194 @@ class Grid(Cell):
         self.s0 = s0
         self.scale_stim_v = scale_stim_v
         self.scale_stim_ip = scale_stim_ip
+
+        # Time
+        self.T = T
+        self.dt = dt
+        self.time = np.linspace(0, T, int(T/dt))
+
+    '''Hofer methods'''
+    def i_rel(self, c, s, ip, r):
+        # Release from ER, including IP3R and leak term [uM/s]
+        return (self.k2 * r * c**2 * ip**2 / (self.ka**2 + c**2) / (self.kip**2 + ip**2)) * (s - c)
+
+    def i_serca(self, c):
+        # SERCA [uM/s]
+        # v_serca = 2
+        # k_serca = 0.1
+        # return v_serca * c / (c + k_serca)
+        return self.k3 * c
+
+    def i_leak(self, c, s):
+        k1 = (self.i_serca(self.c0) - self.i_rel(self.c0, self.s0, self.ip0, self.r0)) / (self.s0 - self.c0)
+        return k1 * (s - c)
+
+    def i_in(self, ip):
+        return 1e9 * (self.i_cal(self.v0, self.n0, self.hv0, self.hc0) + \
+        self.i_cat(self.v0, self.bx0, self.cx0)) / (2 * self.F * self.d) + self.i_out(self.c0) + self.i_pmca(self.c0) + \
+        self.v41 * ip**2 / (self.kr**2 + ip**2) - self.v41 * self.ip0**2 / (self.kr**2 + self.ip0**2)
+
+    def i_pmca(self, c):
+        # PMCA [uM/s]
+        k_pmca = 2.5
+        v_pmca = 4
+        return 0 * v_pmca * c**2 / (c**2 + k_pmca**2)
+
+    def i_out(self, c):
+        # Additional eflux [uM/s]
+        return self.k5 * c
+
+    def v_r(self, c, r):
+        # Rates of receptor inactivation and recovery [1/s]
+        return self.k6 * (self.ki**2 / (self.ki**2 + c**2) - r)
+
+    def i_plcb(self, v8):
+        # Agonist-controlled PLC-beta activity [uM/s]
+        return v8 * 1 / ((1 + self.kg)*(self.kg/(1+self.kg) + self.a0)) * self.a0
+
+    def i_plcd(self, c):
+        # PLC-delta activity [uM/s]
+        return self.v7 * c**2 / (self.kca**2 + c**2)
+
+    def i_deg(self, ip):
+        # IP3 degradion [uM/s]
+        return self.k9 * ip
+
+    def stim(self, t, stims):
+        # Stimulation
+
+        condition = False
+
+        for stim_t in stims:
+            condition = condition or stim_t <= t < stim_t + 4
+
+        if condition:
+            return 1
+        else:
+            return 0
+
+    '''Fast methods'''
+    def i_cal(self, v, n, hv, hc):
+        # L-type calcium channel [mA/cm^2]
+        return self.g_cal * n**2 * hv * hc * (v - self.e_cal)
+
+    def n_inf(self, v):
+        # [-]
+        return 1 / (1 + np.exp(-(v+9)/8))
+
+    def hv_inf(self, v):
+        # [-]
+        return 1 / (1 + np.exp((v+30)/13))
+
+    def hc_inf(self, c):
+        # [-]
+        return self.k_cal / (self.k_cal + c)
+
+    def tau_n(self, v):
+        # [s]
+        return 0.000001 / (1 + np.exp(-(v+22)/308))
+
+    def tau_hv(self, v):
+        # [s]
+        return 0.09 * (1 - 1 / ((1 + np.exp((v+14)/45)) * (1 + np.exp(-(v+9.8)/3.39))))
+
+    def tau_hc(self):
+        # [s]
+        return 0.02
+
+    def i_cat(self, v, bx, cx):
+        return self.g_cat * bx**2 * cx * (v - self.e_cat)
+
+    def bx_inf(self, v):
+        return 1 / (1 + np.exp(-(v+32.1)/6.9))
+
+    def cx_inf(self, v):
+        return 1 / (1 + np.exp((v+63.8)/5.3))
+
+    def tau_bx(self, v):
+        return 0.00045 + 0.0039 / (1 + ((v+66)/26)**2)
+
+    def tau_cx(self, v):
+        return 0.15 - 0.15 / ((1 + np.exp((v-417.43)/203.18))*(1 + np.exp(-(v+61.11)/8.07)))
+    
+    def i_kcnq(self, v, x, z):
+        # KCNQ1 channel [mA/cm^2]
+        return self.g_kcnq * x**2 * z * (v - self.e_k)
+
+    def x_inf(self, v):
+        # [-]
+        return 1 / (1 + np.exp(-(v+7.1)/15))
+
+    def z_inf(self, v):
+        # [-]
+        return 0.55 / (1 + np.exp((v+55)/9)) + 0.45
+
+    def tau_x(self, v):
+        # [s]
+        return 0.001 / (1 + np.exp((v+15)/20))
+
+    def tau_z(self, v):
+        # [s]
+        return 0.01 * (200 + 10 / (1 + 2 * ((v+56)/120)**5))
+
+    def i_kv(self, v, p, q):
+        return self.g_kv * p * q * (v - self.e_k)
+    
+    def p_inf(self, v):
+        return 1 / (1 + np.exp(-(v+1.1)/11))
+
+    def q_inf(self, v):
+        return 1 / (1 + np.exp((v+58)/15))
+
+    def tau_p(self, v):
+        return 0.001 / (1 + np.exp((v+15)/20))
+
+    def tau_q(self, v):
+        return 0.4 * (200 + 10 / (1 + 2*((v+54.18)/120)**5))
+
+    def i_kca(self, v, c):
+        return self.g_kca * 1 / (1 + np.exp(v/(-17) - 2 * np.log(c))) * (v - self.e_k)
+        # return 5 * self.g_kca * c**2 / (c**2 + 5**2) * (v - self.e_k)
+
+    def i_bk(self, v):
+        # Background voltage leak [mA/cm^2]
+        g_bk = - (self.i_cal(self.v0, self.n0, self.hv0, self.hc0) \
+        + self.i_cat(self.v0, self.bx0, self.cx0) \
+        + self.i_kca(self.v0, self.c0))/(self.v0 - self.e_bk)
+        return g_bk * (v - self.e_bk)
+
+    def stim_v(self, t, stims):
+
+        condition = False
+
+        for stim_t in stims:
+            condition = condition or stim_t <= t < stim_t + 0.01
+
+        if condition:
+            return 1
+        else:
+            return 0
+
+    '''Fluorescence methods'''
+    def f_total(self, g, c1g, c2g, c3g, c4g):
+        f_cyt = self.phi0*g + self.phi1*c1g + self.phi2*c2g + \
+        self.phi3*c3g + self.phi4*c4g
+        f_cyt0 = self.phi0*self.g0 + self.phi1*self.c1g0 + \
+        self.phi2*self.c2g0 + self.phi3*self.c3g0 + self.phi4*self.c4g0
+        f_bg = f_cyt0 * 2.5
+        return (f_cyt + f_bg) / (f_cyt0 + f_bg)
+    
+    def r_1(self, c, g, c1g):
+        return self.k1p * c * g - self.k1n * c1g
+    
+    def r_2(self, c, c1g, c2g):
+        return self.k2p * c * c1g - self.k2n * c2g
+    
+    def r_3(self, c, c2g, c3g):
+        return self.k3p * c * c2g - self.k3n * c3g
+
+    def r_4(self, c, c3g, c4g):
+        return self.k4p * c * c3g - self.k4n * c4g
 
     '''Time stepping'''
     def rhs(self, y, t, stims_v, stims_ip):
@@ -83,8 +439,7 @@ class Grid(Cell):
         drdt = self.v_r(c, r)
 
         # IP3 of downstream cells
-        dipdt = iplcb_rest + self.i_plcd(c) - self.i_deg(ip) \
-             + self.g_ip3 * self.L@ip
+        dipdt = iplcb_rest + self.i_plcd(c) - self.i_deg(ip) + self.g_ip3 * self.L@ip
         
         # IP3 of stimulated cells
         dipdt[-int(num/2)-1         : -int(num/2) + 2        ] += iplcb_stim - iplcb_rest
@@ -111,47 +466,47 @@ class Grid(Cell):
         dc4gdt = ir4
 
         # Put together
-        deriv = np.array([dcdt, dsdt, drdt, dipdt, dvdt, dndt, dhvdt, dhcdt, dbxdt, dcxdt, dgdt, dc1gdt, dc2gdt, dc3gdt, dc4gdt])
-        dydt = np.reshape(deriv, 15*self.num2)        
+        dydt = np.concatenate((dcdt, dsdt, drdt, dipdt, dvdt, dndt, dhvdt, dhcdt, dbxdt, dcxdt, dgdt, dc1gdt, dc2gdt, dc3gdt, dc4gdt))  
+
         return dydt
 
-    def step(self, stims_v = [201,203,205,207,209,211,213,215,217,219], stims_ip = [10]):
-        # Time stepping
+def step(model, stims_v = [201,203,205,207,209,211,213,215,217,219], stims_ip = [10]):
+    # Time stepping
 
-        start = time.clock() # Begin counting time
+    start_time = time.time() # Begin counting time
 
-        self.r0 =  self.ki**2 / (self.ki**2 + self.c0**2)
-        self.n0 = self.n_inf(self.v0)
-        self.hv0 = self.hv_inf(self.v0)
-        self.hc0 = self.hc_inf(self.c0)
-        self.bx0 = self.bx_inf(self.v0)
-        self.cx0 = self.cx_inf(self.v0)
-        self.v8 = (self.i_deg(self.ip0) - self.i_plcd(self.c0)) / (1 / ((1 + self.kg)*(self.kg/(1+self.kg) + self.a0)) * self.a0)
+    model.r0 =  model.ki**2 / (model.ki**2 + model.c0**2)
+    model.n0 = model.n_inf(model.v0)
+    model.hv0 = model.hv_inf(model.v0)
+    model.hc0 = model.hc_inf(model.c0)
+    model.bx0 = model.bx_inf(model.v0)
+    model.cx0 = model.cx_inf(model.v0)
+    model.v8 = (model.i_deg(model.ip0) - model.i_plcd(model.c0)) / (1 / ((1 + model.kg)*(model.kg/(1+model.kg) + model.a0)) * model.a0)
 
-        base_mat = np.ones((self.num,self.num))
+    base_mat = np.ones((model.num,model.num))
 
-        inits = [self.c0, self.s0, self.r0, self.ip0, self.v0, self.n0, self.hv0, 
-        self.hc0, self.bx0, self.cx0, self.g0, self.c1g0, self.c2g0, self.c3g0, self.c4g0]
+    inits = [model.c0, model.s0, model.r0, model.ip0, model.v0, model.n0, model.hv0, 
+    model.hc0, model.bx0, model.cx0, model.g0, model.c1g0, model.c2g0, model.c3g0, model.c4g0]
 
-        y0 = np.array([x*base_mat for x in inits])
+    y0 = np.array([x*base_mat for x in inits])
 
-        y0 = np.reshape(y0, 15*self.num2)   
+    y0 = np.reshape(y0, 15*model.num2)   
 
-        sol = odeint(self.rhs, y0, self.time, args = (stims_v, stims_ip), hmax = 0.005)
+    sol = odeint(model.rhs, y0, model.time, args = (np.array(stims_v), np.array(stims_ip)), hmax = 0.005)
 
-        elapsed = (time.clock() - start) # End counting time
-        print("Time used:",elapsed)
+    elapsed = (time.time() - start_time) # End counting time
+    print("Time used:",elapsed)
 
-        return sol
+    return sol
 
 if __name__ == '__main__':
-    n_cel = 5
-    model = Grid(n_cel, 300)
-    sol = model.step()
+    n_cel = num
+    model = Grid(n_cel, 200)
+    sol = step(model)
     # c = np.reshape(sol[:,0:n_cel*n_cel], (-1,n_cel,n_cel))
     # df = pd.DataFrame(np.reshape(c,(-1,n_cel**2)))
     df = pd.DataFrame(sol[:,0:n_cel*n_cel])
-    df.to_csv('../save/data/c_5x5_300s.csv', index = False)
+    df.to_csv('../save/data/c_50x50_200s_withjit.csv', index = False)
 
 
     
