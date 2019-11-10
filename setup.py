82e057873a23c997b1105ac0763c4eddd96c4238
hengjiwang <hengjw@uw.edu>
2019-11-09 22:56:46 -0800

    Reorganize folders

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+import setuptools
+from setuptools import setup 
+
+setup(
+        name='hydracalcium',     
+        version='0.0',   
+        description='A biophysical model of calcium dynamics in Hydra epitheliomuscular cells.',
+        author='Hengji Wang',
+        author_email='hengjw@uw.edu',
+        url='https://github.com/hengjiwang/hydra_calcium_model',
+        packages=setuptools.find_packages()
+)
