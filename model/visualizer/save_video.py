51647eb9962ef22a669f8a95022a04e97c9de1cb
hengjiwang <hengjw@uw.edu>
2019-11-13 10:40:21 -0800

    Modify save video

--- model/visualizer/save_video.py
+++ model/visualizer/save_video.py
@@ -1,45 +1,50 @@
+import cv2, tqdm, sys, os
 import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from visualizer.vlib import save_pattern, save_curve, plot_frame, plot_frames
-import pandas as pd
-import cv2, tqdm, sys, os
+from vlib import save_pattern, save_curve, plot_frame, plot_frames
 
 def save_frames(filename='./new/c_20x20_100s.csv', target='20x20_100s_bending', nx=20, ny=20):
 
-    c = pd.read_csv(filename)
+    c = pd.read_csv(filename).values
     nx = nx
     ny = ny
 
     # domains
-    x = np.linspace(-nx/2,nx/2,nx) 
-    y = np.linspace(-ny/np.pi,ny/np.pi,ny)           
+    x = np.linspace(-nx/2,nx/2,nx)    
+    theta = np.linspace(0, 2*np.pi, ny)
+    y = ny/2/np.pi * np.cos(theta)
+    z = ny/2/np.pi * np.sin(theta)
     X, Y = np.meshgrid(x, y) 
+    X, Z = np.meshgrid(x, z) 
 
     for j in tqdm.tqdm(range(len(c))):
 
-        C = np.reshape(c.values[j], (nx, ny))
-        color = C # change to desired fourth dimension
+        C = np.reshape(c[j], (nx, ny))
+        color = C
+
         norm = matplotlib.colors.Normalize(vmin=0, vmax=0.8)
         m = plt.cm.ScalarMappable(norm=norm, cmap='jet')
         m.set_array([])
-        fcolors = m.to_rgba(color)
 
-        # plot
-        fig = plt.figure(figsize = (10, 10))
+        fcolors = m.to_rgba(color.T)
+
+        fig = plt.figure(figsize=(20, 10))
         ax = fig.gca(projection='3d')
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
         ax.set_xticks([])
         ax.set_yticks([])
         ax.set_zticks([])
-        im = ax.plot_surface(X,Y,np.sqrt((ny/2)**2-Y**2), rstride=1, cstride=1, facecolors=fcolors, vmin = 0, vmax = 0.8, shade=False)
-        ax.view_init(100, 180)
+
+        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.8, edgecolor='k', linewidth=0.5, facecolors=fcolors, vmin=0, vmax=0.8, shade=False)
+
+        # ax.view_init(100, 180)
+        ax.view_init(180, 100)
         plt.savefig('./save/animations/'+target+'/frames/img' + str(j) + '.jpg')
         plt.close(fig)
 
-def save_video(target, ):
+def save_video(target):
     
     file_to_save = './save/animations/'+target+'/movie/movie_4x.avi'
     frames_loc = './save/animations/'+target + '/frames/'
@@ -50,7 +55,7 @@ def save_video(target, ):
     fps = 400
 
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    videoWriter = cv2.VideoWriter(file_to_save,fourcc,fps,(1000,1000))
+    videoWriter = cv2.VideoWriter(file_to_save, fourcc, fps, (1000, 1000))
 
     for i in tqdm.tqdm(range(total_num)):
         frame = cv2.imread(frames_loc+'img'+str(i+1)+'.jpg')
