f6b2a5f97ecddd6056afb21c26e5760e5f8663b8
hengjiwang <hengjw@uw.edu>
2019-11-13 10:50:46 -0800

    Modify save_video input arguments

--- model/visualizer/save_video.py
+++ model/visualizer/save_video.py
@@ -6,14 +6,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from vlib import save_pattern, save_curve, plot_frame, plot_frames
 
-def save_frames(filename='./new/c_20x20_100s.csv', target='20x20_100s_bending', nx=20, ny=20):
+def save_frames(source, target, nx, ny):
 
-    c = pd.read_csv(filename).values
+    c = pd.read_csv(source).values
     nx = nx
     ny = ny
 
     # domains
-    x = np.linspace(-nx/2,nx/2,nx)    
+    x = np.linspace(-nx/2, nx/2, nx)    
     theta = np.linspace(0, 2*np.pi, ny)
     y = ny/2/np.pi * np.cos(theta)
     z = ny/2/np.pi * np.sin(theta)
@@ -37,22 +37,19 @@ def save_frames(filename='./new/c_20x20_100s.csv', target='20x20_100s_bending', 
         ax.set_yticks([])
         ax.set_zticks([])
 
-        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.8, edgecolor='k', linewidth=0.5, facecolors=fcolors, vmin=0, vmax=0.8, shade=False)
-
-        # ax.view_init(100, 180)
+        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.8, edgecolor='k',
+            linewidth=0.5, facecolors=fcolors, vmin=0, vmax=0.8, shade=False)
         ax.view_init(180, 100)
         plt.savefig('./save/animations/'+target+'/frames/img' + str(j) + '.jpg')
         plt.close(fig)
 
-def save_video(target):
+def save_video(target, fps):
     
     file_to_save = './save/animations/'+target+'/movie/movie_4x.avi'
     frames_loc = './save/animations/'+target + '/frames/'
     
     frames = os.listdir(frames_loc)
     total_num = len(frames)
-
-    fps = 400
 
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     videoWriter = cv2.VideoWriter(file_to_save, fourcc, fps, (1000, 1000))
@@ -65,9 +62,13 @@ def save_video(target):
 
 
 if __name__ == '__main__':
-    filename = '~/Downloads/c/c_20x20_100s_elongation.csv'
-    target = '20x20_100s_elongation2'
-    frames_saved = sys.argv[1]
-    if frames_saved == 'False': 
-        save_frames(filename, target, nx=20, ny=20)
-    save_video(target)
+    df = pd.read_json('config.json')
+    source = df.SourceFile.values[0]
+    target = df.TargetFile.values[0]
+    frames_saved = df.FramesSaved.values[0]
+    nx = df.NumX.values[0]
+    ny = df.NumY.values[0]
+    fps = df.TargetFPS.values[0]
+    if frames_saved == 'False':
+        save_frames(source, target, nx, ny)
+    save_video(target, fps)
