1ea1e2722ccb437171d6c3bc75de578c9b55fb7e
hengjiwang <hengjw@uw.edu>
2020-03-18 17:21:34 -0700

    Replace SMC with ProtoSMC

--- hydramuscle/model/smc.py
+++ hydramuscle/model/smc.py
@@ -3,77 +3,74 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-from hydramuscle.model.proto_smc import ProtoSMC
-from hydramuscle.model.fluo_buffer import FluoBuffer
+from hydramuscle.model.fast_cell import FastCell
+from hydramuscle.model.slow_cell import SlowCell
 from hydramuscle.model.euler_odeint import euler_odeint
-from hydramuscle.model.force_encoder import ForceEncoder
 from hydramuscle.model import plot
 
-class SMC(ProtoSMC):
-    """Smooth muscle cell with buffers"""
-    def __init__(self, T=20, dt=0.001, k_ipr=0.05, s0=200, d=40e-4, v_delta=0.03, fluo_ratio=1):
-        super().__init__(T, dt, k_ipr, s0, d, v_delta)
-        self.fluo_buffer = FluoBuffer
-        self.fluo_ratio = fluo_ratio
+class SMC(SlowCell, FastCell):
 
-    def calc_fluo_terms(self, c, g, c1g, c2g, c3g, c4g):
-        ir1 = self.fluo_buffer.r_1(c, g, c1g)
-        ir2 = self.fluo_buffer.r_2(c, c1g, c2g)
-        ir3 = self.fluo_buffer.r_3(c, c2g, c3g)
-        ir4 = self.fluo_buffer.r_4(c, c3g, c4g)
-        dgdt = - ir1
-        dc1gdt = ir1 - ir2
-        dc2gdt = ir2 - ir3
-        dc3gdt = ir3 - ir4
-        dc4gdt = ir4
-        return ir1, ir2, ir3, ir4, dgdt, dc1gdt, dc2gdt, dc3gdt, dc4gdt
+    def __init__(self, T = 20, dt = 0.001, k_ipr = 0.05, s0 = 60, d = 20e-4, v_delta = 0.04, active_v_beta=1):
+        SlowCell.__init__(self, T, dt)
+        FastCell.__init__(self, T, dt)
+
+        self.k_ipr = k_ipr
+        self.s0 = s0
+        self.d = d
+        self.v_delta = v_delta
+        self.alpha = 1e9 / (2 * self.F * self.d)
+        self.active_v_beta = active_v_beta
+
+
+    def i_in(self, ip):
+        return self.alpha * (self.ica0) + self.ipmca0 + self.v_inr * ip**2 / (self.kr**2 + ip**2) - self.in_ip0
+
 
     def rhs(self, y, t, stims_fast, stims_slow):
         # Right-hand side formulation
-        c, s, r, ip, v, m, h, n, g, c1g, c2g, c3g, c4g = y
+        c, s, r, ip, v, m, h, n = y
 
         i_ipr, i_leak, i_serca, i_in, i_pmca, v_r, i_plcd, i_deg = self.calc_slow_terms(c, s, r, ip)
         _, i_ca, i_k, i_bk, dmdt, dhdt, dndt = self.calc_fast_terms(c, v, m, h, n)
-        ir1, ir2, ir3, ir4, dgdt, dc1gdt, dc2gdt, dc3gdt, dc4gdt = self.calc_fluo_terms(c, g, c1g, c2g, c3g, c4g)
 
-        dcdt = i_ipr + i_leak - i_serca + i_in - i_pmca - self.alpha * i_ca + self.fluo_ratio * (- ir1 - ir2 - ir3 - ir4)
+        dcdt = i_ipr + i_leak - i_serca + i_in - i_pmca - self.alpha * i_ca
         dsdt = self.beta * (i_serca - i_ipr - i_leak)
         drdt = v_r
         dipdt = self.i_plcb(self.stim_slow(t, stims_slow, self.active_v_beta)) + i_plcd - i_deg
-        dvdt = - 1 / self.c_m * (i_ca + i_k + i_bk - 0.002 * self.stim_fast(t, stims_fast, dur=0.01))
 
-        return np.array([dcdt, dsdt, drdt, dipdt, dvdt, dmdt, dhdt, dndt, dgdt, dc1gdt, dc2gdt, dc3gdt, dc4gdt])
+        dvdt = - 1 / self.c_m * (i_ca + i_k + i_bk - 0.001 * self.stim_fast(t, stims_fast))
+
+        return np.array([dcdt, dsdt, drdt, dipdt, dvdt, dmdt, dhdt, dndt])
+
 
     def run(self, stims_fast, stims_slow):
         # Run the model
-
         self.init_fast_cell()
         self.init_slow_cell()
 
-        y0 = [self.c0, self.s0, self.r0, self.ip0, self.v0, self.m0, self.h0, self.n0, 
-        self.fluo_buffer.g0, self.fluo_buffer.c1g0, self.fluo_buffer.c2g0, self.fluo_buffer.c3g0, self.fluo_buffer.c4g0]
+        y0 = [self.c0, self.s0, self.r0, self.ip0, self.v0, self.m0, self.h0, self.n0]
+
+        # y = y0
+        # T = self.T
+        # dt = self.dt
 
         sol = euler_odeint(self.rhs, y0, self.T, self.dt, stims_fast=stims_fast, stims_slow=stims_slow)
 
         return sol
 
 if __name__ == '__main__':
-    model = SMC(T=100, dt=0.0002, k_ipr=0.05, s0=200, d=20e-4, v_delta=0.03)
+    model = SMC(T=100, dt=0.0002, k_ipr=0.02, s0=100, d=20e-4, v_delta=0.03)
+
+    ### One Fast Spike ###
     # sol = model.run(stims_fast = [0], stims_slow = [-100])
-    # sol = model.run(stims_fast = list(range(0, 20, 3))+list(range(22, 40, 4)), stims_slow = [-100])
-    sol = model.run(stims_fast = [-100], stims_slow = [10])
-    
-    # g = sol[:,-5]
-    # c1g = sol[:,-4]
-    # c2g = sol[:,-3]
-    # c3g = sol[:,-2]
-    # c4g = sol[:,-1]
-    # fluo = FluoBuffer.f_total(g, c1g, c2g, c3g, c4g)
-    # force = ForceEncoder.encode(c, model.dt)
+    # plot.plot_single_spike(model, sol, 0, 2, 0, 0.05, full_cell=True)
 
-    # plot.plot_single_spike(model, sol, 0, 0.5, 0, 0.05, full_cell=True)
-    plot.plot_slow_transient(model, sol, 0, 100, full_cell=True)
 
-    
+    ### One Slow Transient ###
+    # sol = model.run(stims_fast = [-100], stims_slow = [10])
+    # plot.plot_slow_transient(model, sol, 0, 100, full_cell=True)
+
+    ### Multiple Fast Spikes ###
+    sol = model.run(stims_fast = list(range(0, 50, 3)), stims_slow = [-100])
+    plot.plot_single_spike(model, sol, 0, 60, 0, 60, full_cell=True)
