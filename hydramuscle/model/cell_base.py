1416d4e750aac056e2e905560b125fd74b691cd3
hengjiwang <hengjw@uw.edu>
2020-04-16 13:02:16 -0700

    Delete import pyplot and pandas in cell_base.py

--- hydramuscle/model/cell_base.py
+++ hydramuscle/model/cell_base.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from abc import abstractmethod, ABCMeta
-from tqdm import tqdm
 
 class CellBase(metaclass=ABCMeta):
     '''A base class for simulating single cell dynamics'''
