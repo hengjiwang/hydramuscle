d52086f10aa729095c729842b6039da00b52d90c
Hengji Wang <hengjw@uw.edu>
2020-06-22 09:13:49 -0700

    Add save_shell_force.ipynb

--- hydra/test/manager.bat
+++ hydra/test/manager.bat
@@ -1,5 +1,7 @@
 @echo off
 D:
 cd D:\hydra\hydra\runner
-start python test_shell_cycles.py --T=350
+start python test_shell_cycles.py --save_interval=500 --T=300 --v_delta=0.03
+start python test_shell_cycles.py --save_interval=500 --T=300 --v_delta=0.03 --k_ipr=0.15
+start python test_shell_cycles.py --save_interval=500 --T=300 --v_delta=0.03 --k_ipr=0.1
 exit
