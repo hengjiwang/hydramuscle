a1a52bf265fc1e73003c550752218fe0d9a31a5f
Hengji Wang <hengjw@uw.edu>
2020-09-07 14:51:45 -0700

    Try fit Control EGCaMP video

--- hydra/test/manager.bat
+++ hydra/test/manager.bat
@@ -1,9 +1,4 @@
 @echo off
-D:
-cd D:\hydra\hydra\runner
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=1
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=1
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=1
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=3
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=3
-start python test_shell_random_slowwaves.py --T=100 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112 --randomnum=3
+E:
+cd E:\hydra\hydra\test
+start python test_shell_cycles.py --T=500 --numx=30 --numy=60 --gip3x=0.1 --gip3y=2 --gip3=0.1 --v_delta=0.03 --save_interval=100 --sparsity=0.02 --seed=1112
