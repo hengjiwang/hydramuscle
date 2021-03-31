48e98c1be6d9132bbde80bdc7b0111eddce68049
Hengji Wang <hengjw@uw.edu>
2021-03-30 17:33:52 -0700

    Cancel incorporating v

--- hydramuscle/reduced_model/reduced_fast_cell.py
+++ hydramuscle/reduced_model/reduced_fast_cell.py
@@ -5,31 +5,26 @@ class ReducedFastCell:
 
     def __init__(self, dt=0.001):
         self.dt = dt
-        self.t = 0 # s
+        self.t = 0
         self.c0 = 0.05 # uM
-        self.v0 = 0
         self.tau_stim = 0.05 # s
+        self.r_stim = 1 # uM/s
         self.tau_inc = 0.075 # uM/s
         self.tau_dec = 0.5 # s
         self.tau_ref = 0.1 # s
         self.last_stim = -100 # s
         self.c = self.c0
         self.c_train = [self.c]
-        self.v = self.v0
-        self.v_train = [self.v]
 
     def step(self, stim):
         if stim and self.t - self.last_stim > self.tau_ref:
-            self.v = 1
+            self.c += self.dt / self.tau_inc
             self.last_stim = self.t
         elif self.t - self.last_stim <= self.tau_stim:
-            self.v = 1
-        else:
-            self.v = 0
+            self.c += self.dt / self.tau_inc
 
-        self.c += self.dt * (self.v / self.tau_inc - (self.c - self.c0) / self.tau_dec)
+        self.c -= self.dt * (self.c - self.c0) / self.tau_dec
         self.c_train.append(self.c)
-        self.v_train.append(self.v)
         self.t += self.dt
 
 if __name__ == '__main__':
